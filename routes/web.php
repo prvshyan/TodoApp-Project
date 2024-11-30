e43d840a5cd4836c1a34041d007c0bb1a94659cc
Shayan Alambeigi <alambeigis6@gmail.com>
2024-11-30 17:16:31 +0330

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SignupController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::post('/signup', [SignupController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
