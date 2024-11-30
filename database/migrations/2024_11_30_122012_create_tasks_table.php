dc728ce625024a6dfb8130a96e19068d60bbbbae
Shayan Alambeigi <alambeigis6@gmail.com>
2024-11-30 15:56:57 +0330

    create tables

--- database/migrations/2024_11_30_122012_create_tasks_table.php
+++ database/migrations/2024_11_30_122012_create_tasks_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->boolean('completed')->default(0);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
